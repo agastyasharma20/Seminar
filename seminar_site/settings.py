49f8f177c4bb1664d534d042a68121dce5752386
agastyapy <112243057+agastyapy@users.noreply.github.com>
2026-04-26 20:22:35 +0530

    Fix ALLOWED_HOSTS for seminar.pythonanywhere.com

--- seminar_site/settings.py
+++ seminar_site/settings.py
@@ -4,7 +4,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-this-before-pythonanywhere"
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "testserver", "agastyasharma20.pythonanywhere.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
